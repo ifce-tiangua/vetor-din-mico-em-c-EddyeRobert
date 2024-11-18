acb5a557d6cced341120e15a7d8ab5beac660b48
Eddye <eddyerobert@gmail.com>
2024-11-18 13:31:08 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+int main() {
+    int tamanho;
 
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
+    scanf("%d", &tamanho);
+
+    if (tamanho == 0) {
+        printf("[vazio]\n");
+        return 0;
+    }
+
+    int *vetor = (int *)malloc(tamanho * sizeof(int));
+
+    if (vetor == NULL) {
+        printf("Erro de alocação de memória.\n");
+        return 1;
+    }
+
+    for (int i = 0; i < tamanho; i++) {
+        scanf("%d", &vetor[i]);
+    }
+
+    printf("[");
+    for (int i = 0; i < tamanho; i++) {
+        printf("%d", vetor[i]);
+        if (i < tamanho - 1) {
+            printf(",");
+        }
+    }
+    printf("]\n");
+
+    free(vetor);
 
     return 0;
 }
